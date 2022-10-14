5cb6e83ac2a73802f866cb267782992b5dca0ffa
Reece Harris <reeceharris@email.com>
2022-10-14 11:04:58 +0100

    add extentions

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,18 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    "multiproject",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.httpdomain",
+    "sphinx_tabs.tabs",
+    "sphinx-prompt",
+    "notfound.extension",
+    "hoverxref.extension",
+    "sphinx_search.extension",
+    "sphinxemoji.sphinxemoji",
+    "sphinx_design",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
